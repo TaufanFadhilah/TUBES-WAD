a948e9c88fd792849d8019d15a2028e365898e1f
taftayani <taftayani123@gmail.com>
2017-11-07 11:31:56 +0700

    upadated login

---
+++ Airy/resources/views/content/header.blade.php
@@ -0,0 +1,16 @@
+<style media="screen">
+  #title{
+    margin-left: 20px;
+  }
+</style>
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper blue lighten-3">
+        <a href="#!" class="brand-logo" id="title">Airy</a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="sass.html">Masuk</a></li>
+          <li><a href="badges.html">Daftar</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
