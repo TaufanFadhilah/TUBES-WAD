385ecc443db1813545613860ba673f7dfbbd3cf2
TaufanFadhilah <tfi_23@ymail.com>
2017-11-07 10:15:22 +0700

    Adding branch

--- Airy/resources/views/welcome.blade.php
+++ Airy/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Airy
                 </div>
 
                 <div class="links">
