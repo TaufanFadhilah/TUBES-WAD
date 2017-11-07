c66f6bc33ee410439494a148b6167dc9da3f848d
taftayani <taftayani123@gmail.com>
2017-11-07 10:33:15 +0700

    update frontend

--- Airy/resources/views/welcome.blade.php
+++ Airy/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Airy
+                    Edited by BRD
                 </div>
 
                 <div class="links">
