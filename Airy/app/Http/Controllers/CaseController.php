05c62245b4d987c69d8f340ff870156832d2d09b
TaufanFadhilah <tfi_23@ymail.com>
2017-11-09 19:58:23 +0700

    Adding view files

--- Airy/app/Http/Controllers/CaseController.php
+++ Airy/app/Http/Controllers/CaseController.php
@@ -13,7 +13,7 @@ class CaseController extends Controller
      */
     public function index()
     {
-        //
+        return view('case.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class CaseController extends Controller
      */
     public function create()
     {
-        //
+        return view('case.create');
     }
 
     /**
@@ -45,7 +45,7 @@ class CaseController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('case.show');
     }
 
     /**
@@ -56,7 +56,7 @@ class CaseController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('case.edit');
     }
 
     /**
--- Airy/app/Http/Controllers/DonateController.php
+++ Airy/app/Http/Controllers/DonateController.php
@@ -14,7 +14,7 @@ class DonateController extends Controller
      */
     public function index()
     {
-        //
+        return view('donate.index');
     }
 
     /**
@@ -24,7 +24,7 @@ class DonateController extends Controller
      */
     public function create()
     {
-        //
+        return view('donate.create');
     }
 
     /**
@@ -46,7 +46,7 @@ class DonateController extends Controller
      */
     public function show(Donate $donate)
     {
-        //
+        return view('donate.show');
     }
 
     /**
@@ -57,7 +57,7 @@ class DonateController extends Controller
      */
     public function edit(Donate $donate)
     {
-        //
+        return view('donate.edit');
     }
 
     /**
